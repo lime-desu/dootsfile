31bd7aab358a8f810b44a1e9d56108c0fc3f2ad9
lime <114978689+lime-desu@users.noreply.github.com>
2022-12-09 15:06:20 +0800

    create config directory and add setup script
    setup.sh source:
    https://dev.to/spacerockmedia/how-i-manage-my-dotfiles-using-gnu-stow-4l59

---
+++ config/setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# make sure we have pulled in and updated any submodules
+git submodule init
+git submodule update
+
+# what directories should be installable by all users including the root user
+base=(
+    bash
+)
+
+# folders that should, or only need to be installed for a local user
+useronly=(
+    git
+    zsh
+)
+
+# run the stow command for the passed in directory ($2) in location $1
+stowit() {
+    usr=$1
+    app=$2
+    stow --simulate --verbose --restow --target "${usr}" "${app}"
+}
+
+echo ""
+echo "Stowing apps for user: $(whoami)"
+
+# install apps available to local users and root
+for app in "${base[@]}"; do
+    stowit "${HOME}" "$app" 
+done
+
+if [[ ! "$(whoami)" = *"root"* ]]; then
+    for app in "${useronly[@]}"; do
+        stowit "${HOME}/.config" "$app" 
+    done;
+fi
+
+echo ""
+echo "##### ALL DONE"
