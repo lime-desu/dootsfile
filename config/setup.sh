3271dd438bec067bc5e9fda5776060cc0e233073
lime <114978689+lime-desu@users.noreply.github.com>
2022-12-09 15:56:42 +0800

    add neofetch config

--- config/setup.sh
+++ config/setup.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
+cd $DOOTS
+
 # make sure we have pulled in and updated any submodules
 git submodule init
 git submodule update
 
 # what directories should be installable by all users including the root user
 base=(
-    bash
+    #bash
 )
 
 # folders that should, or only need to be installed for a local user
 useronly=(
-    git
-    zsh
+    config
 )
 
 # run the stow command for the passed in directory ($2) in location $1
@@ -27,12 +28,12 @@ echo "Stowing apps for user: $(whoami)"
 
 # install apps available to local users and root
 for app in "${base[@]}"; do
-    stowit "${HOME}" "$app" 
+    stowit "${XDG_CONFIG_HOME}/" "$app" 
 done
 
 if [[ ! "$(whoami)" = *"root"* ]]; then
     for app in "${useronly[@]}"; do
-        stowit "${HOME}/.config" "$app" 
+        stowit "${XDG_CONFIG_HOME}/" "$app" 
     done;
 fi
 
