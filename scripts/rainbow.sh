2331ce3ec0ce5c775a51076a39794fafd2e4e0c9
lime <114978689+lime-desu@users.noreply.github.com>
2022-12-22 23:03:59 +0800

    add rainbow script and update scripts

---
+++ scripts/rainbow.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# not really a rainbow colors hehe 
+
+red="$(tput setaf 1)"
+grn="$(tput setaf 2)"
+ylw="$(tput setaf 3)"
+blu="$(tput setaf 4)"
+mgn="$(tput setaf 5)"
+cyn="$(tput setaf 6)"
+
+colors=("${ylw}" "${grn}" "${cyn}" "${blu}" "${mgn}" "${red}")
+rst="$(tput sgr0)" 
+bld="$(tput bold)"
+
+echo -n "Enter a message: "
+read message
+
+for (( i=0; i<${#message}; i++ )); do
+  color=${colors[$((i % 6))]}
+  printf '%s%s' "$color" "${message:$i:1}"
+done
+printf "$none\n"
+
