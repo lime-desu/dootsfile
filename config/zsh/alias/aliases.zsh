ade84f6277f494aecfec2f3426629ba3490883aa
lime <114978689+lime-desu@users.noreply.github.com>
2022-12-27 12:24:51 +0800

    add alias
    - since `ls` is aliased to `exa` the `--hidden` flag is not supported
    use `--all` instead

--- config/zsh/alias/aliases.zsh
+++ config/zsh/alias/aliases.zsh
@@ -1,7 +1,7 @@
 alias \
     ls='exa --group-directories-first' \
     ll='ls --long --icons' \
-    l='ll --hidden --icons' \
+    l='ll --all --icons' \
     l.='ls -d .* --icons' \
     ip="ip -human -color addr" \
     myip="ip | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'"
@@ -19,3 +19,6 @@ alias \
     rm='rm -iv' \
     md='mkdir -pv';
     
+alias \
+    update-grub="sudo grub2-mkconfig -o /etc/grub2.cfg && sudo grub2-mkconfig -o /etc/grub2-efi.cfg && sudo grub2-mkconfig -o /boot/grub2/grub.cfg" \
+    reinstall-grub="sudo rm -iv /boot/efi/EFI/fedora/grub.cfg && sudo rm -iv /boot/grub2/grub.cfg; sudo dnf reinstall 'shim-*' 'grub2-efi-*' grub2-common"
