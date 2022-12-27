39b8be8496a15bc28b5afa61e07d9980e1e88eb9
lime <114978689+lime-desu@users.noreply.github.com>
2022-12-27 18:05:32 +0800

    zsh: add clear-screen function
    change the `Ctrl-L` behaviour
    By default, the clear screen keybinding will not clear the backbuffer
    (the part you need to scroll up for to see it) on most terminal emulators.
    This function will solve it clear the buffer

---
+++ config/zsh/functions/clear-screen.zsh
@@ -0,0 +1,11 @@
+function clear-screen-and-scrollback() {
+    echoti civis >"$TTY"
+    printf '%b' '\e[H\e[2J' >"$TTY"
+    zle .reset-prompt
+    zle -R
+    printf '%b' '\e[3J' >"$TTY"
+    echoti cnorm >"$TTY"
+}
+
+zle -N clear-screen-and-scrollback
+bindkey '^L' clear-screen-and-scrollback
