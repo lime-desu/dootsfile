7fe510be5ac02674b481ccd88bb6277aa9f849fa
lime <114978689+lime-desu@users.noreply.github.com>
2022-12-27 20:01:06 +0800

    zsh: add more shell options

--- config/zsh/rc/options.zsh
+++ config/zsh/rc/options.zsh
@@ -1,8 +1,30 @@
- # History
- HISTSIZE=10000000
- SAVEHIST=10000000
- setopt BANG_HIST
- setopt HIST_IGNORE_DUPS
- setopt HIST_IGNORE_ALL_DUPS
- setopt HIST_IGNORE_SPACE
+# History
+HISTSIZE=10000000
+SAVEHIST=10000000
+setopt BANG_HIST
+setopt EXTENDED_HISTORY 
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_FIND_NO_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_VERIFY
+setopt SHARE_HISTORY
 
+# Navigation
+setopt AUTO_CD
+setopt AUTO_PUSHD
+setopt CDABLE_VARS
+setopt PUSHD_IGNORE_DUPS
+setopt PUSHD_SILENT
+
+# Completion
+setopt AUTO_LIST            # Automatically list choices on ambiguous completion.
+setopt CORRECT              # Turn on spelling correction for command.
+setopt COMPLETE_IN_WORD     # Complete from both ends of a word.
+setopt GLOB_COMPLETE        # Show autocompletion menu with globs
+setopt MENU_COMPLETE        # Automatically highlight first element of completion menu
+
+zstyle ':completion:*:descriptions' format '%U%B%d%b%u'
+zstyle ':completion:*:warnings' format '%F{cyan}%BSorry, no matches for: %d%b%f'
