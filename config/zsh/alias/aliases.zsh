4349fedeefbed314ea86d19a71c79ee5b604e147
lime <114978689+lime-desu@users.noreply.github.com>
2022-12-28 11:18:16 +0800

    zsh: add aliases for config file

--- config/zsh/alias/aliases.zsh
+++ config/zsh/alias/aliases.zsh
@@ -20,5 +20,19 @@ alias \
     md='mkdir -pv';
     
 alias \
+    alacrittyrc="$EDITOR $XDG_CONFIG_HOME/alacritty/alacritty.yml" \
+    fzfrc="$EDITOR $XDG_CONFIG_HOME/fzf/fzfrc" \
+    gitrc="$EDITOR $XDG_CONFIG_HOME/git/gitconfig" \
+    gitignorerc="$EDITOR $XDG_CONFIG_HOME/git/gitignore" \
+    hyrlandrc="$EDITOR $XDG_CONFIG_HOME/hypr/hyprland.conf" \
+    kittyrc="$EDITOR $XDG_CONFIG_HOME/kitty/kitty.conf" \
+    nvimrc="$EDITOR $XDG_CONFIG_HOME/after/plugin/defaults.lua" \
+    tmuxrc="$EDITOR $HOME/.tmux.conf.local" \
+    zshrc="$EDITOR $ZDOTDIR/.zshrc" \
+    zshenv="$EDITOR $ZDOTDIR/.zshenv" \
+    {v,vim}="$EDITOR" \
+
+
+alias \
     update-grub="sudo grub2-mkconfig -o /etc/grub2.cfg && sudo grub2-mkconfig -o /etc/grub2-efi.cfg && sudo grub2-mkconfig -o /boot/grub2/grub.cfg" \
     reinstall-grub="sudo rm -iv /boot/efi/EFI/fedora/grub.cfg && sudo rm -iv /boot/grub2/grub.cfg; sudo dnf reinstall 'shim-*' 'grub2-efi-*' grub2-common"
