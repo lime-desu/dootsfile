bcbe0e601e04281f2da8356c7905332f9f172b03
lime <114978689+lime-desu@users.noreply.github.com>
2023-01-08 17:03:23 +0800

    zsh: update flatpak function
    use find instead of ls
    change vars
    create a file if it doesnt exist

--- config/zsh/functions/flatpak.zsh
+++ config/zsh/functions/flatpak.zsh
@@ -1,18 +1,20 @@
-FLATPAK_ALIAS_DIR="$XDG_CONFIG_HOME/zsh/alias/flatpak-aliases.zsh"
+FLATPAK_ALIAS_FILE_DIR="$XDG_CONFIG_HOME/zsh/alias/flatpak-aliases.zsh"
 
 generate_flatpak_alias() {
+  set -e
+  [[ -f $FLATPAK_ALIAS_FILE_DIR ]] && touch $FLATPAK_ALIAS_FILE_DIR
   local flatpak_binaries_dir="/var/lib/flatpak/exports/bin"
-  if test -d "$flatpak_binaries_dir"; then
-    echo -e "\n# Auto-generated aliases for Flatpak applications\n" >> "${FLATPAK_ALIAS_DIR}"
+  if [[ -d "$flatpak_binaries_dir" ]]; then
+    echo -e "\n# Auto-generated aliases for Flatpak applications\n" >> "${FLATPAK_ALIAS_FILE_DIR}"
 
     while read -r app; do
       app_id=$(echo "$app" | awk -F'/' '{print $NF}')
       alias_name=$(echo "$app_id" | awk -F'.' '{print $NF}' | tr '[:upper:]' '[:lower:]')
-      echo "alias $alias_name='flatpak run $app_id'" >> "${FLATPAK_ALIAS_DIR}"
-    done < <(ls "$flatpak_binaries_dir")
+      echo "alias $alias_name='flatpak run $app_id'" >> "${FLATPAK_ALIAS_FILE_DIR}"
+    done < <(find "$flatpak_binaries_dir" -maxdepth 1 -mindepth 1)
 
     echo "Done generating Flatpak aliases"
-    echo "Check $FLATPAK_ALIAS_DIR to modify auto-generated alias"
+    echo "Check $FLATPAK_ALIAS_FILE_DIR to modify auto-generated alias"
   else
     echo "Error: $flatpak_binaries_dir directory does not exist"
   fi
