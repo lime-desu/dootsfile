65a4dd9fb8a82e5f19789a8351761dfec07f5743
lime <114978689+lime-desu@users.noreply.github.com>
2023-01-20 21:51:14 +0800

    tmux: backup tmux.conf if exist don't overwrite it

--- config/zsh/functions/tmux.zsh
+++ config/zsh/functions/tmux.zsh
@@ -1,17 +1,31 @@
 OMT_DIR="${XDG_DATA_HOME:=$HOME/.local/share}/oh-my-tmux"
 omt_install() {
+local omt_doots="$OMT_DIR/.tmux.conf"
+local tmux_doots="${XDG_CONFIG_HOME:-$HOME/.config}/tmux/tmux.conf.local"
     set -e
-    if [ ! -d "$OMT_DIR" ]; then
+    if [ ! -d "$OMT_DIR/.git" ]; then
         mkdir -p "$OMT_DIR"
-        echo "${BLU}Installing ${BLD}${CYN}[Oh My Tmux]${RST} ..."
+        echo "${BLD}${BLU}Installing ${CYN}[Oh My Tmux]${RST} ..."
         git clone https://github.com/gpakosz/.tmux.git "$OMT_DIR"
-        ln -sf "$OMT_DIR/.tmux.conf" "$HOME/.tmux.conf"
-        echo -e "\nSymlinking ${BLD}${CYN}tmux dootsfile${RST} to $HOME..."
-        ln -sf "${XDG_CONFIG_HOME:-$DOOTS/config}/tmux/tmux.conf.local" "$HOME/.tmux.conf.local"
-        tmux source-file "$HOME/.tmux.conf"
+        if [ -f "$omt_doots" ] && [ -f "$tmux_doots" ]; then
+            backup_and_symlink "$HOME/.tmux.conf" "$omt_doots"
+            backup_and_symlink "$HOME/.tmux.conf.local" "$tmux_doots"
+        fi
+    tmux source-file "$HOME/.tmux.conf"
     fi
 }
+
+backup_and_symlink(){
+    if [ -f "$1" ]; then
+        mv -v "$1" "$1.bak"
+        echo -e "Backing up ${BLU}$1...${RST}"
+    fi
+    ln -sf "$2" "$1"
+    echo -e "Creating symlink for ${BLU}$1...${RST}"
+}
+
 omt_install
 
 # to uninstall/remove
 # rm -r $0MT_DIR
+# rm ~/.tmux.conf*
