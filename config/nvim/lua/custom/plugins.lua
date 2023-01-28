6d405f3698070d5f59df339650b7faf7ef8d0620
lime <114978689+lime-desu@users.noreply.github.com>
2023-01-28 15:29:51 +0800

    nvim: remove kickstart.nvim submodule & it's config

--- config/nvim/lua/custom/plugins.lua
+++
@@ -1,9 +0,0 @@
-return function(use)
-  use({
-    "folke/which-key.nvim",
-      config = function()
-        require("which-key").setup({})
-      end
-  })
-  use { "catppuccin/nvim", as = "catppuccin" }
-end
