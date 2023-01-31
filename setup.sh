9e4fdee858b8fc2f2fbfc5d867ee512d77b3954d
lime <114978689+lime-desu@users.noreply.github.com>
2023-01-31 20:38:00 +0800

    setup.sh: update script & add error handling

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 # wip add colors
-# make sure we have pulled in and updated any submodules
+set -e
+
+check_dependency() {
+  if ! command -v "$1" > /dev/null; then
+    echo "Error: '$1' command not found. Please install it first and try again."
+    exit 1
+  fi
+}
+
+check_dependency git 
+check_dependency stow
 
 DOOTS="$HOME/Git/Local/dootsfile"
 CONFIG="$HOME/.config"
@@ -9,19 +19,15 @@ THEMES="$HOME/.local/share/themes"
 ICONS="$HOME/.local/share/icons"
 
 create() {
-  local dir="$1"
-  [ ! -d "$dir" ] && mkdir -p "$dir" && echo "Creating directory for $dir..."
+  local dir="$1" 
+  dir_name="${dir/*\//}"
+  [ ! -d "$dir" ] && mkdir -p "$dir" && echo "Creating directory for '$dir_name'" 
 }
-
-dirs=("$CONFIG" "$BINS" "$THEMES" "$ICONS")
-for dir in "${dirs[@]}"; do
-  create "$dir"
-done
 
 backup() {
   local file="$1"
   if [ -e "$file" ]; then
-    backup_file=${2:-$1.bak}
+    backup_file=${2:-$1.doots}
     if [ ! -e "$backup_file" ]; then
       cp -r "$file" "$backup_file"
       echo "Backing up $file to $backup_file..."
@@ -29,32 +35,39 @@ backup() {
   fi
 }
 
-for file in "$DOOTS"/* ; do 
-  backup "$CONFIG"
-  create "$DOOTS"
+dirs=("$CONFIG" "$BINS" "$THEMES" "$ICONS")
+for dir in "${dirs[@]}"; do
+  backup "$dir"
+  create "$dir"
 done
 
-# wip
-# install() {
-#   cd "$DOOTS" || return
-#   git clone https://github.com/lime-desu/dootsfile.git
-#   git submodule update --init
-# }
-#
-# uninstall() {
-#
-# }
-
-stowit() {
+# If you have this error message below:
+# WARNING! stowing config would cause conflicts:
+# All operations aborted.
+# uncomment `--adopt` flag below
+# and then git reset --hard
+symlink() {
   dootsfile="$1"
   target_dir="$2"
-  stow "$dootsfile" --dir "$DOOTS" --verbose --restow --target "$target_dir"
+  stow "$dootsfile" --dir "$DOOTS" --verbose --restow --target "$target_dir" #--adopt
+  #git reset --hard
 }
 
-stowit config "${CONFIG}"
-stowit bin "${BINS}"
-stowit themes "${THEMES}"
-stowit icons "${ICONS}"
+# wip
+setup() {
+  if [ ! -d "$DOOTS" ]; then
+    create "$DOOTS" && cd "$_" || return
+    git clone --recurse-submodules https://github.com/lime-desu/dootsfile.git "$(pwd)"
+    ./setup.sh
+  fi
+
+  symlink config "${CONFIG}"
+  symlink bin "${BINS}"
+  symlink themes "${THEMES}"
+  symlink icons "${ICONS}"
+}
+
+setup
 
 # For custom installation comment out stowit config from above, and
 # uncomment this line below add/remove config from the array
