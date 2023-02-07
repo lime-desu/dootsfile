248e927941a268cf5264e088ab9b4f597dd2b572
lime <114978689+lime-desu@users.noreply.github.com>
2023-02-07 23:03:36 +0800

    script: tools.sh - don't hardcode --version flag
    some tools doesn't have --version some have -v sometimes -V
    fix: thefuck doesn't show it's version (bug)

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -1,48 +1,21 @@
 #!/usr/bin/env bash
-
 source "${DOOTS}/config/zsh/functions/colors.zsh"
 define_colors
+
 # r̶u̶s̶t̶ linux cli/tui tools and apps that i use
 tools=(alacritty atuin bat broot btop chafa cowsay cava delta dust exa fd foot fuck fzf git kitty librewolf lsd mpv pipes.sh neofetch nvim rg starship tldr topgrade tmux zsh)
-LANG=en_US.UTF-8
-declare -A icons
-icons=(
-  [alacritty]=
-  [atuin]=󰳗
-  [bat]=󰭟
-  [broot]=פּ
-  [btop]=
-  [chafa]=󰈟
-  [cowsay]=
-  [cava]=󰺢
-  [delta]=
-  [dust]=
-  [exa]=
-  [fd]=
-  [foot]=
-  [fuck]=󰚌
-  [fzf]=
-  [git]=
-  [kitty]=
-  [librewolf]=
-  [lsd]=
-  [mpv]=
-  [pipes.sh]=ﳣ
-  [neofetch]=
-  [nvim]=
-  [rg]=
-  [starship]=󱓞
-  [tldr]=
-  [tmux]=
-  [topgrade]=ﮮ
-  [zsh]=
-)
+icons=( 󰳗 󰭟 פּ  󰈟  󰺢      󰚌       ﳣ    󱓞   ﮮ )
+flags=("-V" "-V" "-V" "-V" "-v" "--version" "-V" "-v" "-V" "-V" "-v" "-V" "-v" "-v" "--version" "-v" "-v" "-V" "-V" "-V" "-v" "--version" "--version" "-V" "-V" "-v" "-V" "-V" "--version")
 
 printf "${BLD}%-18s %-10s %-10s\n" "Tools" "Status" "Version"
 printf "%s\n${RST}" "-----------------------------------------------"
-for tool in "${tools[@]}"; do
+for i in "${!tools[@]}"; do
+  tool="${tools[$i]}"
+  icon="${icons[$i]}"
+  version_flag="${flags[$i]}"
+  
   if command -v "$tool" &> /dev/null; then
-    version=$("$tool" --version 2> /dev/null | awk '{
+    version=$("$tool" "$version_flag" 2> /dev/null | awk '{
       for (i = 1; i <= NF; i++) {
         if ($i ~ /[0-9]+\.[0-9]+([.][0-9]+)?/) {
           print $i;
@@ -50,8 +23,8 @@ for tool in "${tools[@]}"; do
         }
       }
     }')
-    printf "${BLU}${icons[$tool]}${RST}${BLD} %-18s ${GRN}%-10s ${RST}${YLW}%-10s\n${RST}" "$tool" "" "$version"
+    printf "${BLU}${icon}${RST}${BLD} %-18s ${GRN}%-10s ${RST}${YLW}%-10s\n${RST}" "$tool" "" "$version"
   else
-    printf "${CYN}${icons[$tool]}${RST}${BLD} %-18s ${RED}%-10s ${BLK}%-10s\n${RST}" "$tool" "" "command not found"
+    printf "${CYN}${icon}${RST}${BLD} %-18s ${RED}%-10s ${BLK}%-10s\n${RST}" "$tool" "" "command not found"
   fi
 done
