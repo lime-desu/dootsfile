c256d33a952de662eb6afb6406f0243fe6032fdf
lime <114978689+lime-desu@users.noreply.github.com>
2023-02-21 22:30:58 +0800

    scripts: fix random-wall (works on all define desktop)

--- scripts/set-random-wallpaper.sh
+++ scripts/set-random-wallpaper.sh
@@ -1,34 +1,31 @@
 #!/usr/bin/env bash
 
 WALLPAPER_DIR=${HOME}/Pictures/Wallpapers/Random_Wallpapers
-RANDOM_PICTURE=$(find "$WALLPAPER_DIR" | shuf -n 1)
+RANDOM_PICTURE=$(find "$WALLPAPER_DIR" | shuf -n 1 | xargs basename)
 CURRENT_DESKTOP="$(echo "$XDG_CURRENT_DESKTOP" | awk '{for (i=1;i<=NF;i++) { $i=toupper(substr($i,1,1)) tolower(substr($i,2)) }}1')"
 
 case "$CURRENT_DESKTOP" in
   Gnome)
     gsettings set org.gnome.desktop.background picture-uri "file://$WALLPAPER_DIR/$RANDOM_PICTURE"
-    gsettings set org.gnome.desktop.background picture-uri-dark "file://$WALLPAPER/$RANDOM_PICTURE"
+    gsettings set org.gnome.desktop.background picture-uri-dark "file://$WALLPAPER_DIR/$RANDOM_PICTURE"
     ;;
   Xfce)
     backdrop=$(xfconf-query --channel xfce4-desktop --property /backdrop --list | grep -E -e "screen.*/monitor.*image-path$" -e "screen.*/monitor.*/last-image$");
     for i in ${backdrop}; do
-        xfconf-query --channel xfce4-desktop --property "$i" --create --type string --set "$WALLPAPER/$RANDOM_PICTURE"
-        xfconf-query --channel xfce4-desktop --property "$i" --set "$WALLPAPER/$RANDOM_PICTURE"
+        xfconf-query --channel xfce4-desktop --property "$i" --create --type string --set "$WALLPAPER_DIR/$RANDOM_PICTURE"
+        xfconf-query --channel xfce4-desktop --property "$i" --set "$WALLPAPER_DIR/$RANDOM_PICTURE"
     done
     ;;
-  KDE)
-    export DISPLAY=:0
-    kwriteconfig5 --file kdeglobals --group Wallpaper --key Picture "$WALLPAPER_DIR/$RANDOM_PICTURE"
-    qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
-        var allDesktops = desktops();
-        print (allDesktops);
-        for (i=0;i<allDesktops.length;i++) {{
-            d = allDesktops[i];
-            d.wallpaperPlugin = "org.kde.image";
-            d.currentConfigGroup = Array("Wallpaper","org.kde.image","General");
-            d.writeConfig("Image", "file:///'$WALLPAPER_DIR/$RANDOM_PICTURE'")
-        }}
-    '
+  Kde)
+    qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "
+      var allDesktops = desktops();
+      print (allDesktops);
+      for (i=0;i<allDesktops.length;i++) {
+        d = allDesktops[i];
+        d.wallpaperPlugin = \"org.kde.image\";
+        d.currentConfigGroup = Array(\"Wallpaper\", \"org.kde.image\", \"General\");
+        d.writeConfig(\"Image\", \"file://${WALLPAPER_DIR}/${RANDOM_PICTURE}\");
+    }"
     ;;
   Sway)
     MODES=(
@@ -39,15 +36,15 @@ case "$CURRENT_DESKTOP" in
       [bg-tile]=tile
     )
     # default to fill if there's no bgtypes    
-    BGTYPE=${BGTYPE:fill}
+    BGTYPE=${BGTYPE:-fill}
     MODE=${MODES[$BGTYPE]}
-    swaymsg output "*" bg "$WALLPAPER" "$MODE"
+    swaymsg output "*" bg "$WALLPAPER_DIR/$RANDOM_PICTURE" "$MODE"
     ;;
-  i3)
+  I3)
     if command -v feh > /dev/null; then
-      feh --bg-scale "$WALLPAPER/$RANDOM_PICTURE"
+      feh --bg-fill "$WALLPAPER_DIR/$RANDOM_PICTURE" && exit
     elif command -v nitrogen > /dev/null; then
-      nitrogen --set-scaled "$WALLPAPER/$RANDOM_PICTURE"
+      nitrogen --set-auto "$WALLPAPER_DIR/$RANDOM_PICTURE" && exit
     fi
     ;;
   *)
@@ -56,6 +53,6 @@ case "$CURRENT_DESKTOP" in
     ;;
 esac
 
-if [[ "$CURRENT_DESKTOP" =~ ^(Gnome|Xfce|KDE|Sway|i3)$ ]]; then
+if [[ "$CURRENT_DESKTOP" =~ ^(Gnome|Xfce|Kde|Sway|I3)$ ]]; then
   notify-send 'Wallpaper changed:' "$RANDOM_PICTURE"
 fi
