f3e6d18c4b0b56a09a72a3bde90bc747f5e40278
lime <114978689+lime-desu@users.noreply.github.com>
2023-03-08 08:42:43 +0800

    script: fzf-rg-launcher add backward compatibility
    afaik supports version 0.30 above
    add v0.38 snippet (become, transform-query)

--- scripts/fzf-rg-launcher.sh
+++ scripts/fzf-rg-launcher.sh
@@ -2,18 +2,35 @@
 
 RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
 INITIAL_QUERY="${*:-}"
-IFS=':' read -ra selected < <(
-  FZF_DEFAULT_COMMAND="$RG_PREFIX $(printf %q "$INITIAL_QUERY")" \
-  fzf --ansi \
-      --color "hl:-1:underline,hl+:-1:underline:reverse" \
-      --disabled --query "$INITIAL_QUERY" \
-      --bind "change:reload:sleep 0.1 && $RG_PREFIX {q} || true" \
-      --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(fzf > )+enable-search+clear-query+rebind(ctrl-r)" \
-      --bind "ctrl-r:unbind(ctrl-r)+change-prompt(rg > )+disable-search+reload($RG_PREFIX {q} || true)+rebind(change,ctrl-f)" \
-      --prompt 'rg > ' \
-      --delimiter : \
-      --header ' Ctrl-r ripgrep mode | Ctrl-f fzf mode ' \
-      --preview 'bat --color=always {1} --highlight-line {2}' \
-      --preview-window 'nohidden,<60(nohidden,up,60%,border-bottom,+{2}+3/3,~3)'
+
+version=$(fzf --version | head -n1 | awk '{print $1}')
+
+fzf_options=(
+  --ansi
+  --color "hl:-1:underline,hl+:-1:underline:reverse"
+  --disabled --query "$INITIAL_QUERY"
+  --bind "change:reload:sleep 0.1 && $RG_PREFIX {q} || true"
+  --prompt '  ripgrep > '
+  --delimiter :
+  --header 'Ctrl-r ripgrep mode | Ctrl-f fzf mode '
+  --preview 'bat --color=always {1} --highlight-line {2}'
+  --preview-window 'nohidden,<60(nohidden,up,60%,border-bottom,+{2}+3/3,~3)'
 )
+
+if [[ "${version}" < "0.37.0" ]]; then
+  IFS=':' read -ra selected < <(
+    FZF_DEFAULT_COMMAND="$RG_PREFIX $(printf %q "$INITIAL_QUERY")" fzf "${fzf_options[@]}" \
+      --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(  fzf > )+enable-search+clear-query+rebind(ctrl-r)" \
+      --bind "ctrl-r:unbind(ctrl-r)+change-prompt(  ripgrep > )+disable-search+reload($RG_PREFIX {q} || true)+rebind(change,ctrl-f)" \
+  )
 [ -n "${selected[0]}" ] && ${EDITOR:-vim} "${selected[0]}" "+${selected[1]}"
+
+else
+  # use this if version >= 38
+  rm -f /tmp/rg-fzf-{r,f}
+    FZF_DEFAULT_COMMAND="$RG_PREFIX $(printf %q "$INITIAL_QUERY")" fzf "${fzf_options[@]}" \
+      --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(  fzf > )+enable-search+rebind(ctrl-r)+transform-query(echo {q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f)" \
+      --bind "ctrl-r:unbind(ctrl-r)+change-prompt(  ripgrep > )+disable-search+reload($RG_PREFIX {q} || true)+rebind(change,ctrl-f)+transform-query(echo {q} > /tmp/rg-fzf-f; cat /tmp/rg-fzf-r)" \
+      --bind "start:unbind(ctrl-r)" \
+      --bind "enter:become(${EDITOR:-vim} {1} +{2})"
+fi
