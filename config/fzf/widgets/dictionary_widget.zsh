fedc576a2060953eab31f1945ceb75b3d28ba8f7
lime <114978689+lime-desu@users.noreply.github.com>
2023-03-08 11:19:24 +0800

    fzf: dictionary_widget reduce dependencies
    remove unecessary packages
    now only requires internet connection and a browser
    (using translate-shell for showing definition)

--- config/fzf/widgets/dictionary_widget.zsh
+++ config/fzf/widgets/dictionary_widget.zsh
@@ -1,22 +1,22 @@
 fzf-dictionary-widget() {
-local dict wiki wikis gogl
-dict="dict {}"
-wiki="wiki {} > /dev/tty"
-wweb="w3m https://en.wikipedia.org/wiki/{}"
-gogl="w3m https://google.com/search?q=define\ {}"
+local _open_
+case "$(uname -s)" in
+  Darwin) _open_="open";;
+  *) _open_="xdg-open";;
+esac
   LBUFFER="$LBUFFER$(FZF_DEFAULT_COMMAND= cat /usr/share/dict/*words | sort | uniq -id | \
     fzf-tmux \
-        -p60% \
+        -p75% \
         --layout=default \
         --header-first \
-        --header="M-w: Wiki | M-d: Define | M-g: Google" \
-        --color=fg:blue,fg+:blue,border:blue \
-        --bind="alt-d:change-preview($dict)" \
-        --bind="alt-w:execute($wiki)" \
-        --bind="alt-g:execute($gogl)" \
-        --prompt=" > " \
-        --preview "$dict" \
-        --preview-window='up,85%,border-bottom,wrap' | paste -sd" " -)"
+        --header="M-w: Wiki | M-g: Google" \
+        --color=fg:blue,fg+:blue,border:blue,preview-label:bold:blue \
+        --bind="alt-w:execute-silent(${_open_} 'https://en.wikipedia.org/wiki/{}')" \
+        --bind="alt-g:execute-silent(${_open_} 'https://google.com/search?q=define {}')" \
+        --prompt=" > " \
+        --preview-label=' Definition ' \
+        --preview "awk -f <(curl -Ls --compressed https://git.io/translate) -- {}" \
+        --preview-window='nohidden,75%,wrap,<50(down,65%,border-top)' | paste -sd" " -)"
   zle reset-prompt
 }
 
