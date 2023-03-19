9e0d3339a949ab1dabb370f04746747e50a24051
lime <114978689+lime-desu@users.noreply.github.com>
2023-03-19 23:36:16 +0800

    nvim: refactor(transparent.lua)
    previous config broke refactor it,
    nvim-transparent plugin are refactored and
    renamed it as transparent.nvim

--- config/nvim/lua/plugins/transparent.lua
+++ config/nvim/lua/plugins/transparent.lua
@@ -1,15 +1,7 @@
--- disable transpareny if inside tmux session
-if vim.env.TERM_PROGRAM == "tmux" then
-  return {
-    {
-      "xiyaowong/nvim-transparent",
-      config = function()
-        require("transparent").setup({
-          enable = true, -- boolean: enable transparent
-          exclude = {}, -- table: groups you don't want to clear
-          ignore_linked_group = true, -- boolean: don't clear a group that links to another group
-        })
-      end,
-    },
-  }
-end
+return {
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    cmd = "TransparentEnable",
+  },
+}
