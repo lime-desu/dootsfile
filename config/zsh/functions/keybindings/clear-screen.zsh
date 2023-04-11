636a10ae7df7dd927ea9f9453e820b8586946fd7
lime <114978689+lime-desu@users.noreply.github.com>
2023-04-11 16:19:44 +0800

    kitty: disable confirmation prompt.
    also fix and refactor(clear-screen)

--- config/zsh/functions/keybindings/clear-screen.zsh
+++ config/zsh/functions/keybindings/clear-screen.zsh
@@ -1,14 +1,17 @@
 clear-screen-and-scrollback() {
-	echoti civis >"$TTY"
-	printf '%b' '\e[H\e[2J' >"$TTY"
+  if [[ "$TERM" == "xterm-kitty" ]]; then
+    printf '%b' '\e[H\e[2J' >"$TTY"
+  else
+    echoti civis >"$TTY"
+    printf '%b' '\e[3J' >"$TTY"
+    echoti cnorm >"$TTY"
+  fi
+
 	(($+commands[wipe])) && wipe --duration 1250
-	zle .reset-prompt
-	zle -R
-	printf '%b' '\e[3J' >"$TTY"
-	echoti cnorm >"$TTY"
+
+  zle reset-prompt
+  zle -R
 }
 
-if [ "$TERM" != "xterm-kitty" ]; then
-	zle -N clear-screen-and-scrollback
-	bindkey '^L' clear-screen-and-scrollback
-fi
+zle -N clear-screen-and-scrollback
+bindkey '^L' clear-screen-and-scrollback
