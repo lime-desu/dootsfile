13bb8f84bdaa6ff974c9543fad89591da462fa6a
lime <114978689+lime-desu@users.noreply.github.com>
2023-04-30 11:03:48 +0800

    fzf(widget): remove/filter out duplicate directory

--- config/fzf/widgets/cd-recent-dir_widget.zsh
+++ config/fzf/widgets/cd-recent-dir_widget.zsh
@@ -2,8 +2,8 @@ fzf-cd-recent-dir-widget () {
   local dir
   print -rNC1 -- $dirstack |
     FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --bind=ctrl-z:ignore ${FZF_DEFAULT_OPTS-} ${FZF_ALT_C_OPTS-}" $(__fzfcmd) +m \
-    --color=fg:bold:blue --query=${LBUFFER} --read0 --print0 |
-    IFS= read -rd '' dir 
+    --color=fg:bold:blue --query=${LBUFFER} --read0 --print0 | awk '{ if (!seen[$0]++) print }' |
+    IFS= read -rd '' dir
     if [[ -n $dir ]]; then
         BUFFER=" builtin cd -- $dir"
         zle accept-line
