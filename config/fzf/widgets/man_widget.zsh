0cb4336a6af294dc1e4156fba5302784897af752
lime <114978689+lime-desu@users.noreply.github.com>
2023-06-03 19:19:40 +0800

    fzf(man_widget): execute man/help directly
    when the fzf-man-widget widget is triggered (pressing alt-h),
    now it would behave similary like zsh run-help builtin.
    
    though, wherein If the buffer is empty, it executes <cmd> man pages.
    In case there is no manual entry, it fallbacks and display --help on a bat pager.
    Otherwise, it opens the fzf widget.
    
    Andalso, some minor improvements, on the widget query.
    Instead of obtaining the entire value of the buffer,
    it now specifically extracts only the last word from it.

--- config/fzf/widgets/man_widget.zsh
+++ config/fzf/widgets/man_widget.zsh
@@ -1,21 +1,31 @@
 fzf-man-widget() {
-  batman="man {1} | col -bx | bat --language=man --plain --color always --theme=\"Monokai Extended\""
-   man -k . | sort \
-   | awk -v CYN=${CYN} -v BLU=${BLU} -v RES=${RES} -v BLD=${BLD} '{ $1=CYN BLD $1; $2=RES BLU;} 1' \
-   | fzf  \
-      -q "$LBUFFER" \
-      --ansi \
-      --tiebreak=begin \
-      --prompt=' Man > '  \
-      --header="M-u: update mandb | M-t: tl;dr | M-c: cheat.sh | M:m manual " \
-      --preview-window '50%,rounded,<50(down,80%,border-up)' \
-      --preview "${batman}" \
-      --bind "enter:execute(man {1})" \
-      --bind "alt-c:+change-preview(curl -s cht.sh/{1})+change-prompt(ﯽ Cheat > )" \
-      --bind "alt-m:+change-preview(${batman})+change-prompt( Man > )" \
-      --bind "alt-u:execute(sudo mandb && echo -e '\nUpdating tl;dr cache...';tldr --update)" \
-      --bind "alt-t:+change-preview(tldr --color=always {1})+change-prompt(ﳁ TLDR > )"
-  zle reset-prompt
+  local cmd="${BUFFER}"
+  local trimmed_cmd="$(echo "$cmd" | awk '{gsub(/^[[:space:]]+|[[:space:]]+$/, ""); print}')"
+  local qry="${trimmed_cmd##* }"
+	# if the buffer has space or empty, open fzf
+	if [[ "$cmd" =~ " " || -z "$cmd" ]]; then
+		batman="man {1} | col -bx | bat --language=man --plain --color always --theme=\"Monokai Extended\""
+		man -k . | sort |
+			awk -v CYN=${CYN} -v BLU=${BLU} -v RES=${RES} -v BLD=${BLD} '{ $1=CYN BLD $1; $2=RES BLU;} 1' |
+			fzf \
+				-q "$qry" \
+				--ansi \
+				--tiebreak=begin \
+				--prompt=' Man > ' \
+				--header="M-u: update mandb | M-t: tl;dr | M-c: cheat.sh | M:m manual " \
+				--preview-window '50%,rounded,<50(down,80%,border-up)' \
+				--preview "${batman}" \
+				--bind "enter:execute(man {1})+clear-query" \
+				--bind "alt-c:+change-preview(curl -s cht.sh/{1})+change-prompt(ﯽ Cheat > )" \
+				--bind "alt-m:+change-preview(${batman})+change-prompt( Man > )" \
+				--bind "alt-u:execute(sudo mandb && echo -e '\nUpdating tl;dr cache...';tldr --update)" \
+				--bind "alt-t:+change-preview(tldr --color=always {1})+change-prompt(ﳁ TLDR > )"
+		zle reset-prompt
+	else
+    # else execute man, if no entry fallback to --help
+    man "$cmd" >/dev/null 2>&1 && man "$cmd" || "$cmd" --help | bat --plain --language=help --paging=always
+	fi
 }
+
 zle     -N          fzf-man-widget
 bindkey '^[h'       fzf-man-widget
