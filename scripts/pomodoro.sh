62c15da400e87bd6cc8c84c1d522cc9077a1bb03
lime <114978689+lime-desu@users.noreply.github.com>
2023-09-13 18:37:59 +0800

    add SafeEyes flatpak package & create autostart
    also fix pomodoro.sh script

--- scripts/pomodoro.sh
+++ scripts/pomodoro.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
+set -x
 gnome-pomodoro --start-stop
 state=$(gsettings get org.gnome.pomodoro.state timer-state-duration)
+
+# need to kill flatpak ps so it doesn't open multiple instance on the status notifier
+enable_safeEyes() { safeeyes --enable || flatpak kill io.github.slgobinath.SafeEyes && flatpak run io.github.slgobinath.SafeEyes --enable; }
+disable_safeEyes() { safeeyes --disable || flatpak kill io.github.slgobinath.SafeEyes && flatpak run io.github.slgobinath.SafeEyes --disable; }
+
 if [[ $state != "0.0" ]]; then
-	safeeyes --enable
 	lofi_ps=$(pgrep -xf "foot sh -c lofi")
 	kill "$lofi_ps"
+	enable_safeEyes
 else
-	safeeyes --disable
+	# run in the bg so it can execute lofi
+	(disable_safeEyes &)
 	foot sh -c lofi
 fi
